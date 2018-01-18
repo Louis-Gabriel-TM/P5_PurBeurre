1de5abdf8afc721a0f934e065b67f07bd5bef454
Louis Gabriel <louis.gabriel@orange.fr>
2018-01-18 12:26:53 +0100

    Create PurBeurre.py and database_JSON_builder.py

---
+++ PurBeurre.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+"""
+Pur Beurre, an application to find healthier products.
+5th project of OC Python Developer Path.
+
+Author: Loïc Mangin
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ database_JSON_builder.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+"""
+Builder of the database used by the Pur Beurre applications.
+5th project of OC Python Developer Path.
+
+Use Open Food Facts API to obtains JSON datas.
+
+Author: Loïc Mangin
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
