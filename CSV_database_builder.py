de6ddd52d6b713f9e783c1a138d27f99ba288e67
Louis Gabriel <louis.gabriel@orange.fr>
2018-02-12 11:18:51 +0100

    Tables creation and data insertion ok

--- CSV_database_builder.py
+++ CSV_database_builder.py
@@ -12,7 +12,13 @@ Author: Loïc Mangin
 
 
 import pandas as pd
+import pymysql
 
+"""
+------------------------------------------------------
+PARSING FUNCTIONS FOR CSV FILES FROM 'OPEN FOOD FACTS'
+------------------------------------------------------
+"""
 
 def add_nutriscore(data):
     """Add a column 'nutriscore' (A, B, C, D, E)
@@ -21,7 +27,6 @@ def add_nutriscore(data):
     """
     score = []
     for row in data.values:
-        print(row[3], row[4])
         score.append(nutriscore(row[3], row[4]))
     data['nutriscore'] = pd.Series(score)
     return data
@@ -61,51 +66,133 @@ def read_CSV_data(file):
     without NaN field,
     extracted from an Open Food Facts CSV file.
     """
-    PB_index = ['url',
-                'product_name',
+    PB_index = ['product_name',
                 'brands',
                 'main_category',
-                'nutrition-score-fr_100g']
+                'nutrition-score-fr_100g',
+                'url']
     data = pd.read_csv(file, sep='\t',
                        usecols=PB_index)
     data = data.dropna()
+    data = data.reset_index(drop=True)
     return data
 
 def format_data(data):
-    """Return the DataFrame with its string fields modified
-    and without NaN field.
+    """Return the DataFrame with its string fields modified.
     """
     data['product_name'] = pd.DataFrame(name.capitalize()
                                     for name in data['product_name'])
     data['brands'] = pd.DataFrame(brand.capitalize()
                                     for brand in data['brands'])
-    data['main_category'] = pd.DataFrame(category.upper()
+    data['main_category'] = pd.DataFrame(category[3:].capitalize()
                                     for category in data['main_category'])
-    data = data.dropna()
     return data
+
+"""
+--------------------------------------------
+BUILDING FUNCTIONS FOR 'PUR BEURRE' DATABASE
+--------------------------------------------
+"""
+
+def connect_db():
+    socket = pymysql.connect(host='localhost',
+                             user='remy',
+                             passwd='ratatouille',
+                             db='PurBeurre_db',
+                             charset='utf8')
+    return socket
+
+def disconnect_db(socket):
+    socket.commit()
+    socket.close()
+
+def create_tables(socket):
+    with socket.cursor() as cursor:
+        cursor.execute("""  DROP TABLE IF EXISTS Saved_substitute; """)
+        cursor.execute("""  DROP TABLE IF EXISTS Product; """)
+        cursor.execute("""  DROP TABLE IF EXISTS Category; """)
+        cursor.execute("""  CREATE TABLE Category (
+                                id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                name VARCHAR(200) NOT NULL,
+                                PRIMARY KEY(id)
+                                )
+                                ENGINE = InnoDB
+                                DEFAULT CHARSET = 'utf8'; """)
+        cursor.execute("""  CREATE TABLE Product (
+                                id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                id_category TINYINT UNSIGNED NOT NULL,
+                                name VARCHAR(200) NOT NULL,
+                                main_category VARCHAR(200) NOT NULL,
+                                brands VARCHAR(200) NOT NULL,
+                                score_100g TINYINT NOT NULL,
+                                nutriscore CHAR(1) NOT NULL,
+                                url VARCHAR(200) NOT NULL,
+                                PRIMARY KEY(id),
+                                CONSTRAINT fk_category_id
+                                    FOREIGN KEY(id_category)
+                                    REFERENCES Category(id)
+                                )
+                                ENGINE = InnoDB
+                                DEFAULT CHARSET = 'utf8'; """)
+        cursor.execute("""  CREATE TABLE Saved_substitute (
+                                id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                                id_product BIGINT UNSIGNED NOT NULL,
+                                id_substitute BIGINT UNSIGNED NOT NULL,
+                                backup_date DATE NOT NULL,
+                                PRIMARY KEY(id),
+                                CONSTRAINT fk_product_id
+                                    FOREIGN KEY(id_product)
+                                    REFERENCES Product(id),
+                                CONSTRAINT fk_substitute_id
+                                    FOREIGN KEY(id_substitute)
+                                    REFERENCES Product(id)
+                                )
+                                ENGINE = InnoDB
+                                DEFAULT CHARSET = 'utf8'; """)
+
+def insert_category(socket, category):
+    with socket.cursor() as cursor:
+        cursor.execute(""" INSERT INTO Category (name)
+                               VALUES ('{}'); """
+                       .format(category))
+
+def insert_product(socket, category, product):
+    with socket.cursor() as cursor:
+        cursor.execute("""  INSERT INTO Product (id_category,
+                                                 url,
+                                                 name,
+                                                 brands,
+                                                 main_category,
+                                                 score_100g,
+                                                 nutriscore)
+                                VALUES ((SELECT id FROM Category
+                                             WHERE name = "{0}"),
+                                        "{1}", "{2}", "{3}", "{4}", {5}, "{6}"); """
+                       .format(category, product[0], product[1], product[2], product[3], product[4], product[5]))
+    socket.commit()
 
 
 def main():
-    """categories = ['aliments-origine-végétale',
-                  'biscuits-gâteaux',
-                  'boissons',
-                  'céréales-pommes-terre',
-                  'desserts',
-                  'plats-préparés',
-                  'produits-laitiers',
-                  'snacks-sucrés',
-                  'viandes'
-                  ]
-                  """
     categories =['boissons']
+
+    # PARSING CSV FILES FROM 'OPEN FOOD FACTS'
     for category in categories:
         PB_data = read_CSV_data(category + '.csv')
-
         PB_data = format_data(PB_data)
-
         PB_data = add_nutriscore(PB_data)
 
-    print(PB_data[:5])
+    # BUILDING 'PUR BEURRE' DATABASE
+    db = connect_db()
+    create_tables(db)
+    for category in categories:
+        insert_category(db, category)
+    for product in PB_data.values:
+        print(product)
+        insert_product(db, 'boissons', product)
+        for j in range(6):
+            print(product[j])
+        print()
+    disconnect_db(db)
 
 
 if __name__ == "__main__":
--- PB_constants.py
+++ PB_constants.py
@@ -9,20 +9,29 @@ Graphic Constants used by the Pur Beure appplication.
 Author: Loïc Mangin
 """
 
-option_1_text = "1. Je souhaite trouver un substitut à un aliment"
-option_2_text = "2. Je souhaite revoir les substituts que j'ai enregistré"
-ask_text = "Votre choix : "
+OPT_1_TEXT = "Je souhaite trouver un substitut à un aliment"
+OPT_2_TEXT = "Je souhaite revoir les substituts que j'ai enregistré"
+
+PB_CATEGORIES = ['aliments-origine-végétale',
+                 'biscuits-gateaux',
+                 'boissons',
+                 'céréales-pommes-terre',
+                 'desserts',
+                 'plats-préparés',
+                 'produits-laitiers',
+                 'snacks-sucrés',
+                 'viandes']
 
 # CORPORATE IDENTITY AND STYLE GUIDE CONSTANTS
 
-pb_brand = "Pur Beurre"
-pb_headline = "Du gras, oui, mais de qualité !"
-pb_logo = "images/pb_logo.png"
+PB_BRAND = "Pur Beurre"
+PB_HEADLINE = "Du gras, oui, mais de qualité !"
+PB_LOGO = "images/pb_logo.png"
 
 
 # Pur Beurre Colors
-font_color_1 = '#C45525' # color 'Rooibos chocolat'
-font_color_2 = '#345A61' # color 'Moules frites'
-bonbon = '#86EBE6'
-brand_color_1 = '#DE9440' # color 'Caramel mou'
-brand_color_2 = '#E8A75D' # color 'Biscuit trempé'
+FONT_COLOR_1 = '#C45525' # color 'Rooibos chocolat'
+FONT_COLOR_2 = '#345A61' # color 'Moules frites'
+BONBON_COLOR = '#86EBE6'
+BRAND_COLOR_1 = '#DE9440' # color 'Caramel mou'
+BRAND_COLOR_2 = '#E8A75D' # color 'Biscuit trempé'
