73894b8a157b71c5385d11938839ee40463e4470
Louis Gabriel <louis.gabriel@orange.fr>
2018-02-15 08:02:56 +0100

    Database builder ok, PEP 8/PEP 257 ok

--- CSV_database_builder.py
+++ CSV_database_builder.py
@@ -3,18 +3,20 @@
 
 
 """
-Script building the database used by the Pur Beurre application
-from the Open Food Facts data in CSV format.
-3rd project of OC Python Developer Path.
+Script building the database for the 'Pur Beurre' application
+using the Open Food Facts data in CSV format.
+
+5th project of OC Python Developer Path.
 
 Author: Loïc Mangin
 """
 
 
+from PB_constants import *
+
 import pandas as pd
 import pymysql
 
-from PB_constants import *
 
 """
 ------------------------------------------------------
@@ -22,10 +24,10 @@ PARSING FUNCTIONS FOR CSV FILES FROM 'OPEN FOOD FACTS'
 ------------------------------------------------------
 """
 
+
 def add_nutriscore(dataframe):
-    """Add a column 'nutriscore' (A, B, C, D, E)
-    based on 'main_category' (index=4)
-    and 'nutrition-score-fr_100g' (index=5) fields.
+    """Add a column 'nutriscore' (A, B, C, D, E) based on
+    'main_category' (index=4) and 'nutrition-score-fr_100g' (index=5) fields.
     """
     score = []
     for row in dataframe.values:
@@ -33,8 +35,23 @@ def add_nutriscore(dataframe):
     dataframe['nutriscore'] = pd.Series(score)
     return dataframe
 
+
+def format_csv_data(dataframe):
+    """Return the DataFrame with some string fields modified.
+    """
+    dataframe['product_name'] = pd.DataFrame(name.capitalize()
+                                             for name in dataframe['product_name'])
+    dataframe['brands'] = pd.DataFrame(brand.capitalize()
+                                       for brand in dataframe['brands'])
+    dataframe['main_category'] = pd.DataFrame(category[3:].capitalize()
+                                              for category in dataframe['main_category'])
+    return dataframe
+
+
 def nutriscore(main_category, score):
-    """Find the used scales at
+    """Return the 'nutriscore' (A, B, C, D, E) based on
+    the value of 'nutrition-score-fr_100g'.
+    The used scales can be find at :
     https://fr.openfoodfacts.org/score-nutritionnel-experimental-france
     """
     if main_category.upper().find('BOISSON') != -1:
@@ -62,48 +79,49 @@ def nutriscore(main_category, score):
         else:
             return 'E'
 
-def parse_CSV_data(dataframe):
+
+def parse_csv_data(dataframe):
+    """Return a dataframe containing only products in the right
+    categories and without too long strings.
+    The brand 'Fleury Michon' is excluded due to escapement issues
+    in its strings fields.
+    """
     pb_categories = {category.lower() for category in PB_CATEGORIES}
     for i in dataframe.index:
-        categories = dataframe.loc[i,'categories'].split(",")
+        categories = dataframe.loc[i, 'categories'].split(",")
         categories = {category.lower().strip() for category in categories}
-        if pb_categories & categories != set():
-            new_cat = str(list(pb_categories & categories)[0])
-        else:
+        if pb_categories & categories == set()\
+                or len(dataframe.loc[i, 'product_name']) > 50\
+                or len(dataframe.loc[i, 'main_category']) > 100\
+                or len(dataframe.loc[i, 'brands']) > 50\
+                or len(dataframe.loc[i, 'url']) > 100\
+                or dataframe.loc[i, 'brands'] in ['Fleury Michon']:
             new_cat = None
+        else:
+            new_cat = str(list(pb_categories & categories)[0])
         dataframe.at[i, 'categories'] = new_cat
     dataframe = dataframe.dropna()
     dataframe = dataframe.reset_index(drop=True)
     return dataframe
 
-def read_CSV_data(file):
-    """Return a DataFrame containing the
-    useful data for Pur Beurre application
-    without NaN field,
+
+def read_csv_data(file):
+    """Return a DataFrame containing the useful data for the 'Pur Beurre'
+    application without NaN / NULL field,
     extracted from an Open Food Facts CSV file.
     """
-    PB_index = ['url',
+    pb_index = ['url',
                 'product_name',
                 'brands',
                 'categories',
                 'main_category',
                 'nutrition-score-fr_100g']
     dataframe = pd.read_csv(file, sep='\t',
-                       usecols=PB_index)
+                            usecols=pb_index)
     dataframe = dataframe.dropna()
     dataframe = dataframe.reset_index(drop=True)
     return dataframe
 
-def format_CSV_data(dataframe):
-    """Return the DataFrame with its string fields modified.
-    """
-    dataframe['product_name'] = pd.DataFrame(name.capitalize()
-                                    for name in dataframe['product_name'])
-    dataframe['brands'] = pd.DataFrame(brand.capitalize()
-                                    for brand in dataframe['brands'])
-    dataframe['main_category'] = pd.DataFrame(category[3:].capitalize()
-                                    for category in dataframe['main_category'])
-    return dataframe
 
 """
 --------------------------------------------
@@ -111,7 +129,10 @@ BUILDING FUNCTIONS FOR 'PUR BEURRE' DATABASE
 --------------------------------------------
 """
 
+
 def connect_db():
+    """Return an access to the 'Pur Beurre' database previously created.
+    """
     socket = pymysql.connect(host='localhost',
                              user='remy',
                              passwd='ratatouille',
@@ -119,18 +140,17 @@ def connect_db():
                              charset='utf8')
     return socket
 
-def disconnect_db(socket):
-    socket.commit()
-    socket.close()
 
 def create_tables(socket):
+    """Drop existing tables of the 'Pur Beurre' database and recreate them.
+    """
     with socket.cursor() as cursor:
         cursor.execute("""  DROP TABLE IF EXISTS Saved_substitute; """)
         cursor.execute("""  DROP TABLE IF EXISTS Product; """)
         cursor.execute("""  DROP TABLE IF EXISTS Category; """)
         cursor.execute("""  CREATE TABLE Category (
                                 id TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
-                                name VARCHAR(200) NOT NULL,
+                                name VARCHAR(50) NOT NULL,
                                 PRIMARY KEY(id)
                                 )
                                 ENGINE = InnoDB
@@ -138,13 +158,12 @@ def create_tables(socket):
         cursor.execute("""  CREATE TABLE Product (
                                 id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
                                 id_category TINYINT UNSIGNED NOT NULL,
-                                name VARCHAR(200) NOT NULL,
-                                category VARCHAR(200) NOT NULL,
-                                family VARCHAR(200) NOT NULL,
-                                brands VARCHAR(200) NOT NULL,
+                                name VARCHAR(50) NOT NULL,
+                                subcategory VARCHAR(100) NOT NULL,
+                                brands VARCHAR(50) NOT NULL,
                                 score_100g TINYINT NOT NULL,
                                 nutriscore CHAR(1) NOT NULL,
-                                url VARCHAR(200) NOT NULL,
+                                url VARCHAR(100) NOT NULL,
                                 PRIMARY KEY(id),
                                 CONSTRAINT fk_category_id
                                     FOREIGN KEY(id_category)
@@ -168,60 +187,63 @@ def create_tables(socket):
                                 ENGINE = InnoDB
                                 DEFAULT CHARSET = 'utf8'; """)
 
+
+def disconnect_db(socket):
+    """Push the modifications in the 'Pur Beurre' database
+    and close access to it.
+    """
+    socket.commit()
+    socket.close()
+
+
 def insert_category(socket, category):
+    """Fill the 'Category' table of 'Pur Beurre' database.
+    """
     with socket.cursor() as cursor:
         cursor.execute(""" INSERT INTO Category (name)
                                VALUES ("{}"); """
                        .format(category))
 
-def insert_product(socket, category, product):
+
+def insert_product(socket, product):
+    """Fill the 'Product' table of 'Pur Beurre' database.
+    """
     with socket.cursor() as cursor:
         cursor.execute("""  INSERT INTO Product (id_category,
                                                  url,
                                                  name,
                                                  brands,
-                                                 category,
-                                                 family,
+                                                 subcategory,
                                                  score_100g,
                                                  nutriscore)
                                 VALUES ((SELECT id FROM Category
                                              WHERE name = "{0}"),
                                         "{1}", "{2}", "{3}", "{4}", {5}, "{6}"); """
-                       .format(category, product[0], product[1], product[2], product[3], product[4], product[5], product[6]))
+                       .format(product[3], product[0], product[1], product[2], product[4], product[5], product[6]))
     socket.commit()
 
 
 def main():
-    #for category in PB_CATEGORIES:
-    #    print(category)
+    # --- PARSING CSV FILES FROM 'OPEN FOOD FACTS' ---
+    pb_data = read_csv_data('fr.openfoodfacts.org.products.csv')
+    # pb_data = read_CSV_data('boissons.csv')
+    pb_data = parse_csv_data(pb_data)
+    pb_data = format_csv_data(pb_data)
+    pb_data = add_nutriscore(pb_data)
 
-
-    # PARSING CSV FILES FROM 'OPEN FOOD FACTS'
-    PB_data = read_CSV_data('boissons.csv')
-    #print(PB_data[:5])
-    PB_data = parse_CSV_data(PB_data)
-    #print(PB_data[:5])
-
-    PB_data = format_CSV_data(PB_data)
-    PB_data = add_nutriscore(PB_data)
-    print(PB_data[:5])
-
-    # BUILDING 'PUR BEURRE' DATABASE
+    # --- BUILDING 'PUR BEURRE' DATABASE ---
     db = connect_db()
     create_tables(db)
-
     for category in PB_CATEGORIES:
         insert_category(db, category)
-    """
-    for product in PB_data.values:
+    count = 0
+    for product in pb_data.values:
+        count += 1
+        print(count)
         print(product)
-        insert_product(db, 'boissons', product)
-        for j in range(6):
-            print(product[j])
+        insert_product(db, product)
         print()
-    """
     disconnect_db(db)
-
 
 
 if __name__ == "__main__":
--- PB_constants.py
+++ PB_constants.py
@@ -4,6 +4,7 @@
 
 """
 Graphic Constants used by the Pur Beure appplication.
+
 5th project of OC Python Developer Path.
 
 Author: Loïc Mangin
@@ -13,25 +14,29 @@ OPT_1_TEXT = "Je souhaite trouver un substitut à un aliment"
 OPT_2_TEXT = "Je souhaite revoir les substituts que j'ai enregistré"
 
 PB_CATEGORIES = {"Aliments d'origine végétale",
-                 "Biscuits et gâteaux",
                  "Boissons",
-                 "Céréales et pommes de terre",
+                 "Céréales et dérivés",
                  "Desserts",
                  "Plats préparés",
+                 "Produits de la mer",
                  "Produits laitiers",
                  "Snacks sucrés",
                  "Viandes"}
 
-# CORPORATE IDENTITY AND STYLE GUIDE CONSTANTS
+
+"""
+--------------------------------------------
+CORPORATE IDENTITY AND STYLE GUIDE CONSTANTS
+--------------------------------------------
+"""
 
 PB_BRAND = "Pur Beurre"
 PB_HEADLINE = "Du gras, oui, mais de qualité !"
 PB_LOGO = "images/pb_logo.png"
 
-
-# Pur Beurre Colors
-FONT_COLOR_1 = '#C45525' # color 'Rooibos chocolat'
-FONT_COLOR_2 = '#345A61' # color 'Moules frites'
+# --- 'PUR BEURRE' COLORS ---
+FONT_COLOR_1 = '#C45525'  # color 'Rooibos chocolat'
+FONT_COLOR_2 = '#345A61'  # color 'Moules frites'
 BONBON_COLOR = '#86EBE6'
-BRAND_COLOR_1 = '#DE9440' # color 'Caramel mou'
-BRAND_COLOR_2 = '#E8A75D' # color 'Biscuit trempé'
+BRAND_COLOR_1 = '#DE9440'  # color 'Caramel mou'
+BRAND_COLOR_2 = '#E8A75D'  # color 'Biscuit trempé'
