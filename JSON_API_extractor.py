1f67e808673eb74ce449716f2729bfa27e5714a9
Louis Gabriel <louis.gabriel@orange.fr>
2018-02-19 13:03:08 +0100

    Update JSON_API_extractor, PEP 8 / PEP 257 ok

--- JSON_API_extractor.py
+++ JSON_API_extractor.py
@@ -3,10 +3,10 @@
 
 
 """
-Builder of the database used by the Pur Beurre application.
-5th project of OC Python Developer Path.
+Script requesting Open Food Facts API to obtains a few JSON data.
+The aim is to show this approach takes to much time to get entire database.
 
-Use Open Food Facts API to obtains JSON datas.
+5th project of OC Python Developer Path.
 
 Author: Loïc Mangin
 """
@@ -16,124 +16,86 @@ import json
 import requests
 import time
 
-def estimate_total_duration(max_requests, duration):
+
+def products_total_nb():
+    """Return the number of products referenced in the Open Food Facts
+    french database, based on data in 'categories.json' file.
+    """
     with open('categories.json', 'r') as f:
         categories = json.load(f)
-    products_nb = 0
-    for i in range(categories['count']):
-        products_nb += categories['tags'][i]['products']
-    estimated_requests_nb = products_nb // 20
-    estimated_total_duration = round((estimated_requests_nb / max_requests) * duration, 1)
-    print("Nb of products: " + str(products_nb))
-    print("Nb of requests: " + str(estimated_requests_nb))
-    print("Total duration of JSON extraction: " + str(estimated_total_duration / 3600) + " h")
+    products_count = 0
+    for category in categories['tags']:
+        products_count += category['products']
+    return products_count
+
+
+def format_duration(duration):
+    """Return the duration in the format '{HH} h {MM} min {SS.S} s'.
+    """
+    hours = int(duration // 3600)
+    minutes = int((duration % 3600) // 60)
+    seconds = round(duration % 60, 1)
+    return "{0} h {1} min {2} s".format(hours, minutes, seconds)
+
+
+def format_int(n):
+    """Return the integer n in a '3-digits' format, with n < 1 000 000.
+    """
+    return str(n // 1000) + " " + str(n % 1000)
+
 
 def get_categories():
+    """Get all french categories, with some details, in a JSON file
+    from the Open Food Facts database.
+    """
     url = 'https://fr.openfoodfacts.org/categories.json'
-    datas = requests.get(url).json()
+    data = requests.get(url).json()
     with open('categories.json', 'w') as f:
-        f.write(json.dumps(datas, indent=4))
+        f.write(json.dumps(data, indent=4))
 
-def get_product_from_categories(min_products, max_requests):
+
+def get_products(max_requests):
+    """Do a limited number of requests, each one getting Open Food Facts
+     data for 20 products (or less) in JSON format.
+     """
     with open('categories.json', 'r') as f:
         categories = json.load(f)
-
-    requests_nb = 0
-
+    requests_count = 0
     with open('products.json', 'w') as f:
-        pass
-
-    for i in range(3):
-        products_nb = categories['tags'][i]['products']
-        url = categories['tags'][i]['url']
-        if products_nb >= min_products:
-            for j in range(1, products_nb // 20):
-                if requests_nb >= max_requests:
-                    break
-                request_url = url + "/" + str(j) + ".json"
-                print(request_url)
-                datas = requests.get(request_url).json()
-                requests_nb += 1
-
-                with open('products.json', 'a') as f:
-                    f.write(json.dumps(datas, indent=4))
-
-    print("Nb of requests: " + str(requests_nb))
-
-def get_product_with_barcode(url):
-    datas = requests.get(url).json()
-    with open('granola.json', 'w') as f:
-        f.write(json.dumps(datas, indent=4))
-
-def get_product_with_name(name):
-    datas = requests.get('https://fr.openfoodfacts.org/cgi/search.pl?\
-                         search_terms=' + name + \
-                         '&search_simple=1&action=process&json=1').json()
-    with open(name + '.json', 'w') as f:
-        f.write(json.dumps(datas, indent=4))
-
-def display_product_with_name(name):
-    with open(name + '.json', 'r') as f:
-        products = json.load(f)
-    for product in products['products']:
-        print(product['product_name_fr'], end=' ')
-        print('[code : ' + product['code'] + ']', end=' >>> ')
-        print(product['generic_name'])
-        print(product['brands'])
-        print('>>> Nutriscore : ' + product['nutrition_grades'].upper())
-        print('>>> ' + product['stores'])
-        print('>>> ' + product['url'])
-        print()
-
-def select_main_categories():
-    with open('categories.json') as f:
-        categories = json.load(f)
-    min_products = 2000
-    i = 0
-    while categories['tags'][i]['products'] >= min_products:
-        print(categories['tags'][i]['name'])
-        i += 1
-    print(i - 1)
+        for category in categories['tags']:
+            products_nb = category['products']
+            url = category['url']
+            for i in range(1, products_nb // 20 + 1):
+                if requests_count < max_requests:
+                    request_url = url + "/" + str(i) + ".json"
+                    print(request_url)
+                    data = requests.get(request_url).json()
+                    f.write(json.dumps(data, indent=4))
+                    requests_count += 1
+    print()
+    print("Nombre de requêtes effectuées : " + str(requests_count))
 
 
 def main():
-    #get_categories()
-    min_products = 2000
-    max_requests = 100
+    get_categories()
+    # --- DO A LIMITED NUMBER OF REQUESTS ---
+    requests_nb = 5  # Fixed umber of requests to OFF API
+    t_start = time.clock()
+    get_products(requests_nb)
+    t_end = time.clock()
+    # --- ESTIMATE DURATION FOR REQUESTING ENTIRE DATABASE ---
+    duration = t_end - t_start
+    print("Durée des requêtes effectuées : " + format_duration(duration))
+    print()
+    prod_total = products_total_nb()
+    print("Nombre total de produits dans la base : " + format_int(prod_total))
+    req_total = prod_total // 20
+    print("Estimation (basse) du nombre de requêtes "
+          "nécessaires : " + format_int(req_total))
+    total_duration = duration * (req_total / requests_nb)
+    print("Estimation (basse) de la durée de récupération "
+          "de la base complète : " + format_duration(total_duration))
 
-    start_time = time.time()
-    get_product_from_categories(min_products, max_requests)
-    end_time = time.time()
-    duration = round(end_time - start_time, 1)
-    print(str(duration) + " s")
-
-    estimate_total_duration(max_requests, duration)
-
-
-"""
-import json
-import urllib.request
-
-# https://world.openfoodfacts.org/product/3029330003458
-def main():
-    for i in range(2, 5):
-        url = "https://fr.openfoodfacts.org/categorie/boissons/" + str(i) + ".json"
-        data = json.load(urllib.request.urlopen(url))
-        print('DATA >>> ' + str(data))
-        try:
-            product = data['products'][0]
-            print('Code : ' + product['code'])
-            print('Nom : ' + product['product_name_fr'].upper())
-            print('>>>> Marque : ' + product['brands'].capitalize())
-            print('>> Catégories : ' + ', '.join(product['categories'].split(',')))
-            print('>>>> NutriScore : ' + product['nutrition_grade_fr'].upper())
-            print('>>>> Scanné à ' + product['stores'].capitalize() + ' - ' + product['purchase_places'])
-            print("Plus d'info sur" + url[:-5])
-            print()
-
-        except:
-            pass
-"""
 
 if __name__ == "__main__":
     main()
