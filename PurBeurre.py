20d83fd092d3f594bb12c99a158d907e6da3e2b5
Louis Gabriel <louis.gabriel@orange.fr>
2018-02-28 11:48:53 +0100

    Some improvements and PEP 8 / PEP 257 standardization

--- PurBeurre.py
+++ PurBeurre.py
@@ -12,28 +12,23 @@ Author: Loïc Mangin
 
 
 from classes.category import *
-from classes.product import *
 from classes.saved_substitute import *
 from PB_constants import *
 
-from datetime import datetime
-
 import pymysql
+from random import randint
 
 
-def ask_for_home():
-    print()
-    option = choose_option("Souhaitez-vous revenir à la page d'accueil ?",
-                  option_1="Retourner à l'accueil",
-                  option_2="Quitter")
-    if option == 1:
-        return True
-    else:
-        return False
+"""
+------------------
+DATABASE FUNCTIONS
+------------------
+"""
 
 
 def db_connect():
-    """Return an access to the 'Pur Beurre' database previously created.
+    """Return an access to the 'Pur Beurre' database
+    previously created with CSV_database_builder.py.
     """
     socket = pymysql.connect(host='localhost',
                              user='remy',
@@ -43,27 +38,140 @@ def db_connect():
     return socket
 
 
-def choose_line(listing, text, alt):
-    choice = input(text)
-    print()
-    if not (1 <= int(choice) <= len(listing)):
-        print(alt)
-        print()
-        choice = choose_line(listing, text, alt)
-    return int(choice)
+def db_disconnect(socket):
+    """Push the modifications in the 'Pur Beurre' database
+    and close access to it.
+    """
+    socket.commit()
+    socket.close()
 
 
-def choose_product(excerpt):
-    choice = input("Produit choisi : ")
-    print()
-    if not (1 <= int(choice) <= len(excerpt)):
-        print("Ce produit n'est pas proposé.")
-        print()
-        choice = choose_product(excerpt)
-    return int(choice)
+def get_categories(socket):
+    """Get categories from 'Pur Beurre' datbase.
+    Return a list of Category instances.
+    """
+    cat_list = []
+    with socket.cursor() as cursor:
+        cursor.execute("""  SELECT * FROM Category; """)
+        cat_tuples = cursor.fetchall()
+    for cat in cat_tuples:
+        category = Category(cat[0], cat[1])
+        cat_list.append(category)
+    return cat_list
+
+
+def get_excerpt(socket, cat_id, nutriscore, length):
+    """Get some products from 'Pur Beurre' database
+    randomly selected and with a 'bad' nutriscore.
+    Return a list of Product instances.
+    """
+    prod_list = []
+    with socket.cursor() as cursor:
+        cursor.execute("""  SELECT * FROM Product
+                                WHERE id_category = {0}
+                                AND nutriscore >= '{1}'; """
+                       .format(cat_id, nutriscore))
+        prod_tuples = cursor.fetchall()
+    index = random_index(length, prod_tuples)
+    prod_tuples = [prod_tuples[i] for i in index]
+    prod_tuples.sort(key=lambda tup: tup[2])
+    for prod in prod_tuples:
+        product = Product(prod[0], prod[1], prod[2], prod[3],
+                          prod[4], prod[5], prod[6], prod[7])
+        prod_list.append(product)
+    return prod_list
+
+
+def get_saved_substitutes(socket, nb):
+    """Get last saved researches (product and substitute)
+    from 'Pur Beurre' database.
+    Return a list of Saved_substitute instances.
+    """
+    saved_list = []
+    with socket.cursor() as cursor:
+        cursor.execute("""  SET lc_time_names = 'fr_FR'; """)
+        cursor.execute("""  SELECT id, id_product, id_substitute, 
+                                    DATE_FORMAT(backup_date, '%W %e %M %Y') 
+                                FROM Saved_substitute
+                                ORDER BY backup_date DESC
+                                LIMIT {0}; """.format(nb))
+        saved_tuples = cursor.fetchall()
+    for saved_sub in saved_tuples:
+        saved_substitute = Saved_substitute(saved_sub[0], saved_sub[1],
+                                            saved_sub[2], saved_sub[3])
+        saved_list.append(saved_substitute)
+    return saved_list
+
+
+def get_substitutes(socket, product, length):
+    """Get some randomly selected possible substitutes (better nutriscore).
+    Return a list of Product instances.
+    """
+    sub_list = []
+    with socket.cursor() as cursor:
+        cursor.execute("""  SELECT * FROM Product
+                                WHERE subcategory = '{0}' 
+                                AND nutriscore < '{1}'; """
+                       .format(product.subcategory, product.nutriscore))
+        sub_tuples = cursor.fetchall()
+    index = random_index(length, sub_tuples)
+    sub_tuples = [sub_tuples[i] for i in index]
+    sub_tuples.sort(key=lambda tup: tup[2])
+    for sub in sub_tuples:
+        substitute = Product(sub[0], sub[1], sub[2], sub[3],
+                             sub[4], sub[5], sub[6], sub[7])
+        sub_list.append(substitute)
+    return sub_list
+
+
+def random_index(length, tup):
+    """Return a list of randomly selected index of a tuple.
+    """
+    index = []
+    while len(index) != min(length, len(tup)):
+        i = randint(0, len(tup) - 1)
+        if i not in index:
+            index.append(i)
+    return index
+
+
+def save_substitute(socket, product, substitute):
+    """Save a research (product and substitute)
+    in the 'Pur Beurre' database.
+    """
+    with socket.cursor() as cursor:
+        cursor.execute("""  INSERT INTO Saved_substitute (id_product, 
+                                                          id_substitute,
+                                                          backup_date)
+                                VALUES ({0}, {1}, CURRENT_DATE()); """
+                       .format(product.id, substitute.id))
+    print("Produit et substitut sauvegardés.")
+
+
+"""
+-------------------
+INTERFACE FUNCTIONS
+-------------------
+"""
+
+
+def ask_for_home():
+    """Return True if user wants to return to home page.
+    """
+    print("\n" + "-" * 50 + "\n")
+    option = choose_option("Souhaitez-vous revenir à la page d'accueil ?",
+                           option_1="Retourner à l'accueil",
+                           option_2="Quitter")
+    if option == 1:
+        return True
+    else:
+        return False
 
 
 def choose_option(text, option_1, option_2):
+    """Propose an alternative amongst 2 choices.
+    Test answer validity and return choice.
+    """
     print(text)
     print("  1. " + option_1)
     print("  2. " + option_2)
@@ -76,21 +184,57 @@ def choose_option(text, option_1, option_2):
     return int(choice)
 
 
-def db_disconnect(socket):
-    """Push the modifications in the 'Pur Beurre' database
-    and close access to it.
+def choose_line(listing, text, alt):
+    """Propose to choose an option in a listing.
+    Test answer validity and return choice.
     """
-    socket.commit()
-    socket.close()
+    choice = input(text)
+    print()
+    if not (1 <= int(choice) <= len(listing)):
+        print(alt)
+        print()
+        choice = choose_line(listing, text, alt)
+    return int(choice)
+
+
+def choose_product(excerpt):
+    """Return the integer representing the chosen product
+    in the excerpt.
+    """
+    choice = input("Produit choisi : ")
+    print()
+    if not (1 <= int(choice) <= len(excerpt)):
+        print("Ce produit n'est pas proposé.")
+        print()
+        choice = choose_product(excerpt)
+    return int(choice)
 
 
 def display_categories(categories):
+    """Display the list of categories
+    using the 'display' method of Category class.
+    """
     for category in categories:
         category.display()
 
 
+def display_excerpt(prod_list, head):
+    """Display a list of 'Product.name' instances
+    (with 'nutriscore' and 'score_100g' to debug).
+    """
+    print("--- " + head.upper() + " ---")
+    i = 0
+    for product in prod_list:
+        i += 1
+        print("  " + str(i) + "." + " " * (2 - (i // 10)) + product.name
+              + " (" + product.nutriscore + ", " + str(product.score_100g) + ")"
+              )
+
+
 def display_head():
-    head_length = max(len(PB_BRAND), len (PB_HEADLINE))
+    """Display program header with brand and headline.
+    """
+    head_length = max(len(PB_BRAND), len(PB_HEADLINE))
     print()
     print("#" * (head_length + 8))
     print("#   " + PB_BRAND.upper().center(head_length) + "   #")
@@ -99,73 +243,11 @@ def display_head():
     print()
 
 
-def display_excerpt(prod_list, head):
-    print("--- " + head.upper() + " ---")
-    i = 0
-    for product in prod_list:
-        i += 1
-        print("  " + str(i) + ". " + product.name + " ("
-              + product.nutriscore + ", " + str(product.score_100g) + ")")
-
-
-def get_saved_substitutes(socket):
-    saved_list = []
-    with socket.cursor() as cursor:
-        cursor.execute("""  SELECT * FROM Saved_substitute
-                                LIMIT 3; """)
-        saved_tuples = cursor.fetchall()
-    for saved_sub in saved_tuples:
-        saved_substitute = Saved_substitute(saved_sub[0], saved_sub[1], saved_sub[2], saved_sub[3])
-        saved_list.append(saved_substitute)
-    return saved_list
-
-
-def get_substitutes(socket, product):
-    sub_list = []
-    with socket.cursor() as cursor:
-        cursor.execute("""  SELECT * FROM Product
-                                WHERE subcategory = '{0}' AND nutriscore < '{1}'
-                                LIMIT 5; """.format(product.subcategory, product.nutriscore))
-        sub_tuples = cursor.fetchall()
-    for sub in  sub_tuples:
-        substitute = Product(sub[0], sub[1], sub[2], sub[3], sub[4], sub[5], sub[6], sub[7])
-        sub_list.append(substitute)
-    return sub_list
-
-
-def get_categories(socket):
-    cat_list = []
-    with socket.cursor() as cursor:
-        cursor.execute("""  SELECT * FROM Category; """)
-        cat_tuples = cursor.fetchall()
-    for cat in cat_tuples:
-        category = Category(cat[0], cat[1])
-        cat_list.append(category)
-    return cat_list
-
-
-def get_excerpt(socket, cat_id):
-    prod_list = []
-    with socket.cursor() as cursor:
-        cursor.execute("""  SELECT * FROM Product
-                                WHERE id_category = {0} AND nutriscore != 'A'
-                                LIMIT 10; """.format(cat_id))
-        prod_tuples = cursor.fetchall()
-    for prod in prod_tuples:
-        #print(prod)
-        product = Product(prod[0], prod[1], prod[2], prod[3], prod[4], prod[5], prod[6], prod[7])
-        prod_list.append(product)
-        #product.display()
-    return prod_list
-
-
-def save_substitute(socket, product, substitute):
-    with socket.cursor() as cursor:
-        cursor.execute("""  INSERT INTO Saved_substitute (id_product, 
-                                                          id_substitute,
-                                                          backup_date)
-                                VALUES ({0}, {1}, CURRENT_DATE()); """
-                       .format(product.id, substitute.id))
+"""
+-------------
+MAIN FUNCTION
+-------------
+"""
 
 
 def main():
@@ -174,9 +256,9 @@ def main():
     while go_home:
         # --- HOME PAGE ---
         display_head()
-        option = choose_option(text="En quoi 'Pur Beurre' peut-il vous aider ?",
-                               option_1=OPT_1_TEXT,
-                               option_2=OPT_2_TEXT)
+        option = choose_option(text=HOME_ASK,
+                               option_1=HOME_1_TEXT,
+                               option_2=HOME_2_TEXT)
         if option == 1:
             # --- OPTION 1: Find a substitute ---
             # 1.1. Chose category
@@ -184,28 +266,28 @@ def main():
             print("--- CATEGORIES DISPONIBLES ---")
             display_categories(categories)
             chosen_cat = choose_line(categories,
-                                     text="Catégorie choisie : ",
-                                     alt="Cette catégorie n'existe pas.")
+                                     text=CATEGORY_ASK,
+                                     alt=CATEGORY_ALT)
             # 1.2. Choose product
-            excerpt = get_excerpt(db, chosen_cat)
-            display_excerpt(excerpt, "Quelques produits issus de cette catégorie")
+            excerpt = get_excerpt(db, chosen_cat, 'C', 10)
+            display_excerpt(excerpt, PRODUCT_HEAD)
             chosen_prod = choose_line(excerpt,
-                                         text="Produit choisi : ",
-                                         alt="Ce produit n'est pas proposé.")
+                                      text=PRODUCT_ASK,
+                                      alt=PRODUCT_ALT)
             # 1.3. Show substitutes
-            results = get_substitutes(db, excerpt[chosen_prod - 1])
-            if results == []:
-                print(">>> Désolé, 'Pur Beurre' n'a pu vous trouver aucun substitut <<<")
+            results = get_substitutes(db, excerpt[chosen_prod - 1], 5)
+            if not results:
+                print(NO_RESULT)
             else:
-                display_excerpt(results, "Substituts proposés")
+                display_excerpt(results, SUBSTITUTE_HEAD)
                 # 1.4. Show more informations on a product
                 chosen_substitute = choose_line(results,
-                                                text="Sur quel substitut souhaité vous davantage d'informations : ",
-                                                alt="Ce substitut n'est pas proposé.")
+                                                text=SUBSTITUTE_ASK,
+                                                alt=SUBSTITUTE_ALT)
                 results[chosen_substitute - 1].display()
-                option = choose_option(text="Souhaitez-vous sauvegarder ce résultat ?",
-                                       option_1="Sauvegarder le substitut",
-                                       option_2="Ne pas sauvegarder")
+                option = choose_option(text=SAVE_ASK,
+                                       option_1=SAVE_1_TEXT,
+                                       option_2=SAVE_2_TEXT)
                 if option == 1:
                     # 1.5a. Save substitute
                     product = excerpt[chosen_prod - 1]
@@ -214,7 +296,8 @@ def main():
         else:
             # --- OPTION 2: View saved substitues
             # 2.1. Show saved substitutes
-            saved_substitutes = get_saved_substitutes(db)
+            saved_substitutes = get_saved_substitutes(db, 3)
+            print(saved_substitutes)
             for saved_sub in saved_substitutes:
                 saved_sub.display(db)
             # 2.2. Ask for return to home
